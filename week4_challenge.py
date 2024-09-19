08ce2b17dcd27c519ab125dd9ba559ccdf5848e9
Nowak <vnowak@cps.k12.il.us>
2024-09-19 12:24:01 -0500

    added new folder

---
+++ week4_challenge.py
@@ -0,0 +1,13 @@
+# Extracting Sub-Strings Practice #1
+# Extract the first word of the following sentence using slicing, and display it on the screen:
+text = "Controlling complexity is the essence of programming"
+# Hint: "Controlling" is 11 characters long.
+print(text[0:12])
+# Extracting Sub-Strings Practice #2
+# Take every third character starting from the ninth to the end of the sentence, and print the result.
+words ="Never trust a computer you can't throw out a window"
+print(words[9::3])
+# Extracting Sub-Strings Practice #3
+# Reverses the position of all the characters in the following sentence and displays the result on the screen.
+texts = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+print(texts[::-1])
